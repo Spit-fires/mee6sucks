cd83a124ab082e9a9fc8010620ee80b170c5f297
akashfahad12@gamil.com <akashfahad12@gamil.com>
2022-09-30 23:25:01 +0600

    testimonials final

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-const wrapper = document.querySelector('.wrapper');
-const indicators = [...document.querySelectorAll('.indicators .button')];
-
-let currentTestimonial = 0; // Default 0
-
-indicators.forEach((item, i) => {
-  item.addEventListener('click', () => {
-    indicators[currentTestimonial].classList.remove('active');
-    wrapper.style.marginLeft = `-${100 * i}%`;
-    item.classList.add('active');
-    currentTestimonial = i;
-  })
-})
 
 
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
